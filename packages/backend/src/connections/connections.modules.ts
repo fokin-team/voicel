8c947fa1fa9235452e2028a28af62141c7bd3589
MagicFun1241 <nganzikov@gmail.com>
2022-11-26 15:05:10 +0000

    fix(backend): try to fix build issue

--- packages/backend/src/connections/connections.modules.ts
+++ packages/backend/src/connections/connections.modules.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { WsModule } from '@/ws/ws.module';
 import { ConnectionsGateway } from "./connections.gateway";
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [WsModule],
+    imports: [WsModule, ConfigService],
     providers: [ConnectionsGateway],
 })
 export class ConnectionsModule {}
