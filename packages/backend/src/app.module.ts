988b16352a568a5bef6c0485a3eb56caf44f935b
MagicFun1241 <nganzikov@gmail.com>
2022-11-26 14:44:25 +0000

    fix(backend): fix connections module import

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { WsModule } from '@/ws/ws.module';
 import { ConfigModule } from '@nestjs/config';
+import { ConnectionsModule } from './connections/connections.modules';
 
 @Module({
   imports: [
     WsModule,
     ConfigModule.forRoot(),
+    ConnectionsModule,
   ],
   controllers: [],
 })
