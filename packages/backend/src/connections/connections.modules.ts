648272127d93c0bd842c515a59094422642d8db9
MagicFun1241 <nganzikov@gmail.com>
2022-11-26 15:16:46 +0000

    chore(backend): linux build for mkcert

--- packages/backend/src/connections/connections.modules.ts
+++ packages/backend/src/connections/connections.modules.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { WsModule } from '@/ws/ws.module';
 import { ConnectionsGateway } from "./connections.gateway";
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [WsModule, ConfigService],
+    imports: [WsModule, ConfigModule],
     providers: [ConnectionsGateway],
 })
 export class ConnectionsModule {}
