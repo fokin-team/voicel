06b64be79adb7b1f3da297d03c8ce26939267519
pont1s <maxmonsta@gmail.com>
2022-11-26 09:11:42 +0300

    feat(web-app): VDotLoading init

---
+++ packages/web-app/src/components/kit/VDotLoading.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="v-dot-loading">
+    <div class="text">
+      <slot name="default" />
+    </div>
+    <div class="ellipsis" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
