7d18903d7dbf629f5e7d4a8c0b12c431ba106146
pont1s <maxmonsta@gmail.com>
2022-11-26 21:33:49 +0300

    feat(web-app): implement initTransports

--- packages/web-app/src/utils/WebRTC.ts
+++ packages/web-app/src/utils/WebRTC.ts
@@ -2,7 +2,6 @@ import { Device, types as MediasoupTypes } from 'mediasoup-client';
 
 import { WebSocketProvider } from '@/utils/WebSocket';
 import { Emitter } from '@/utils/shared/Emitter';
-import { RtpCapabilities } from 'mediasoup-client/lib/RtpParameters';
 
 class WebRtc {
   public name: string;
@@ -17,11 +16,15 @@ class WebRtc {
 
   public roomId = '';
 
+  public producerTransport: MediasoupTypes.Transport | undefined = undefined;
+
+  public consumerTransport: MediasoupTypes.Transport | undefined = undefined;
+
   public consumers = new Map();
 
   public producers = new Map();
 
-  public device: Device | null = null;
+  public device: Device | undefined = undefined;
 
   public isOpen = false;
 
@@ -42,19 +45,25 @@ class WebRtc {
   }
 
   async createRoom() {
-    const room = await this.ws.emitPromised('create-room');
-    console.log(room);
-  }
-
-  async join() {
-    const room = await this.ws.emitPromised('join');
-    console.log(room);
-    const data = await this.ws.emitPromised<MediasoupTypes.RtpCapabilities>('get-rtp-capabilities');
-    if (!data.status) {
-      throw new Error(data.errors as string);
+    const createRoomResponse = await this.ws.emitPromised<{ roomId: string }>('create-room');
+    if (!createRoomResponse.status) {
+      throw new Error(createRoomResponse.errors as string);
     }
 
-    this.device = await this.loadDevice(data.data);
+    this.roomId = createRoomResponse.data.roomId;
+
+    return this.roomId;
+  }
+
+  async join(roomId: string) {
+    const room = await this.ws.emitPromised('join', { roomId });
+    console.log(room);
+    const responseRtpCapabilities = await this.ws.emitPromised<MediasoupTypes.RtpCapabilities>('get-rtp-capabilities');
+    if (!responseRtpCapabilities.status) {
+      throw new Error(responseRtpCapabilities.errors as string);
+    }
+
+    this.device = await this.loadDevice(responseRtpCapabilities.data);
   }
 
   async loadDevice(routerRtpCapabilities: MediasoupTypes.RtpCapabilities) {
@@ -74,9 +83,106 @@ class WebRtc {
   }
 
   async initTransports() {
+    const responseCreateWebRtcProducerTransport = await this.ws
+      .emitPromised<MediasoupTypes.TransportOptions>('create-rtc-transport', {
+      forceTcp: false,
+      rtpCapabilities: this.device?.rtpCapabilities,
+    });
 
+    if (!responseCreateWebRtcProducerTransport.status) {
+      throw new Error(responseCreateWebRtcProducerTransport.errors as string);
+    }
+
+    this.producerTransport = this.device?.createSendTransport(responseCreateWebRtcProducerTransport.data);
+
+    this.producerTransport?.on('connect', async ({ dtlsParameters }, callback, errorCallback) => {
+      const response = await this.ws.emitPromised('connect-transport', {
+        dtlsParameters,
+        transportId: this.producerTransport?.id,
+      });
+
+      if (!response.status) {
+        errorCallback(new Error(response.errors as string));
+        throw new Error(response.errors as string);
+      }
+
+      callback();
+    });
+
+    this.producerTransport?.on('produce', async ({ kind, rtpParameters }, callback, errorCallback) => {
+      const response = await this.ws.emitPromised<{ producerId: string }>('produce', {
+        producerTransportId: this.producerTransport?.id,
+        kind,
+        rtpParameters,
+      });
+
+      if (!response.status) {
+        errorCallback(new Error(response.errors as string));
+        throw new Error(response.errors as string);
+      }
+
+      callback({ id: response.data.producerId });
+    });
+
+    this.producerTransport?.on('connectionstatechange', (state) => {
+      switch (state) {
+        case 'connecting':
+          break;
+
+        case 'connected':
+          break;
+
+        case 'failed':
+          this.producerTransport?.close();
+          break;
+
+        default:
+          break;
+      }
+    });
+
+    const responseCreateWebRtcConsumerTransport = await this.ws
+      .emitPromised<MediasoupTypes.TransportOptions>('create-rtc-transport', {
+      forceTcp: false,
+    });
+
+    if (!responseCreateWebRtcConsumerTransport.status) {
+      throw new Error(responseCreateWebRtcConsumerTransport.errors as string);
+    }
+
+    this.consumerTransport = this.device?.createRecvTransport(responseCreateWebRtcConsumerTransport.data);
+
+    this.consumerTransport?.on('connect', async ({ dtlsParameters }, callback, errorCallback) => {
+      const response = await this.ws.emitPromised('connect-transport', {
+        dtlsParameters,
+        transportId: this.consumerTransport?.id,
+      });
+
+      if (!response.status) {
+        errorCallback(new Error(response.errors as string));
+        throw new Error(response.errors as string);
+      }
+
+      callback();
+    });
+
+    this.consumerTransport?.on('connectionstatechange', (state) => {
+      switch (state) {
+        case 'connecting':
+          break;
+
+        case 'connected':
+          break;
+
+        case 'failed':
+          this.consumerTransport?.close();
+          break;
+
+        default:
+          break;
+      }
+    });
   }
-
 
   initSockets() {
 
@@ -106,7 +212,7 @@ class WebRtc {
 
   }
 
-  removeConsumer(consumerId: string){
+  removeConsumer(consumerId: string) {
 
   }
 
@@ -116,10 +222,8 @@ class WebRtc {
   async getRoomInfo() {
 
   }
-
-
 }
 
 export {
   WebRtc,
-}
+};
