464299644897f09c18b849ee2e79658367a20043
pont1s <maxmonsta@gmail.com>
2022-11-26 12:28:51 +0300

    chore(backend): replace express on fastify

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,37 @@
+import * as fs from 'fs';
+
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { Logger } from '@nestjs/common';
+import { WsAdapter } from '@/ws-adapter/ws-adapter.adapter';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const httpsOptions = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === undefined ? {
+    key: fs.readFileSync(`${process.cwd()}/tools/mkcert/key.pem`),
+    cert: fs.readFileSync(`${process.cwd()}/tools/mkcert/cert.pem`),
+  } : undefined;
+
+  Logger.log(`Process ENV: ${process.env.NODE_ENV}`, 'ENV');
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+      https: httpsOptions,
+    }),
+  );
+
+  app.enableCors();
+
+  app.useWebSocketAdapter(new WsAdapter(app));
+
+  await app.listen(3000, '0.0.0.0');
 }
 
-bootstrap();
+// eslint-disable-next-line no-void
+void bootstrap();
