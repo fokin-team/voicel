978383286cfaa05759e5a0a4ab779ca3e13f1d8c
Danila Fokin <sir.sotoros@yandex.ru>
2022-11-27 08:47:36 +0300

    feat(cicd): Add env and new config

--- packages/backend/src/mediasoup/mediasoup.config.ts
+++ packages/backend/src/mediasoup/mediasoup.config.ts
@@ -4,42 +4,154 @@ import {
   TransportListenIp,
 } from 'mediasoup/node/lib/types';
 
+import os from 'os';
+
 export const config = {
   // http server ip, port, and peer timeout constant
   httpIp: '0.0.0.0',
   httpPort: 3000,
   httpPeerStale: 360000,
 
-  mediasoup: {
-    worker: {
-      rtcMinPort: 40000,
-      rtcMaxPort: 49999,
-      logLevel: 'debug',
-      logTags: ['info', 'ice', 'dtls', 'rtp', 'srtp', 'rtcp'] as WorkerLogTag[],
+  // mediasoup settings.
+  mediasoup:
+  {
+    // Number of mediasoup workers to launch.
+    numWorkers: Object.keys(os.cpus()).length,
+    // mediasoup WorkerSettings.
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#WorkerSettings
+    workerSettings:
+    {
+      logLevel: 'warn',
+      logTags:
+        [
+          'info',
+          'ice',
+          'dtls',
+          'rtp',
+          'srtp',
+          'rtcp',
+          'rtx',
+          'bwe',
+          'score',
+          'simulcast',
+          'svc',
+          'sctp'
+        ],
+      rtcMinPort: process.env.MEDIASOUP_MIN_PORT || 40000,
+      rtcMaxPort: process.env.MEDIASOUP_MAX_PORT || 49999
     },
-    router: {
-      mediaCodecs: [
-        {
-          kind: 'audio',
-          mimeType: 'audio/opus',
-          clockRate: 48000,
-          channels: 2,
-        },
-      ] as RtpCodecCapability[],
+    // mediasoup Router options.
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#RouterOptions
+    routerOptions:
+    {
+      mediaCodecs:
+        [
+          {
+            kind: 'audio',
+            mimeType: 'audio/opus',
+            clockRate: 48000,
+            channels: 2
+          },
+          {
+            kind: 'video',
+            mimeType: 'video/VP8',
+            clockRate: 90000,
+            parameters:
+            {
+              'x-google-start-bitrate': 1000
+            }
+          },
+          {
+            kind: 'video',
+            mimeType: 'video/VP9',
+            clockRate: 90000,
+            parameters:
+            {
+              'profile-id': 2,
+              'x-google-start-bitrate': 1000
+            }
+          },
+          {
+            kind: 'video',
+            mimeType: 'video/h264',
+            clockRate: 90000,
+            parameters:
+            {
+              'packetization-mode': 1,
+              'profile-level-id': '4d0032',
+              'level-asymmetry-allowed': 1,
+              'x-google-start-bitrate': 1000
+            }
+          },
+          {
+            kind: 'video',
+            mimeType: 'video/h264',
+            clockRate: 90000,
+            parameters:
+            {
+              'packetization-mode': 1,
+              'profile-level-id': '42e01f',
+              'level-asymmetry-allowed': 1,
+              'x-google-start-bitrate': 1000
+            }
+          }
+        ]
     },
-
-    // rtp listenIps are the most important thing, below. you'll need
-    // to set these appropriately for your network for the demo to
-    // run anywhere but on localhost
-    webRtcTransport: {
-      listenIps: [
-        {
-          ip: process.env.WEBRTC_LISTEN_IP || '0.0.0.0',
-          announcedIp: process.env.A_IP || '127.0.0.1',
-        },
-      ] as TransportListenIp[],
-      maxIncomingBitrate: 1500000,
-      initialAvailableOutgoingBitrate: 800000,
+    // mediasoup WebRtcServer options for WebRTC endpoints (mediasoup-client,
+    // libmediasoupclient).
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcServerOptions
+    // NOTE: mediasoup-demo/server/lib/Room.js will increase this port for
+    // each mediasoup Worker since each Worker is a separate process.
+    webRtcServerOptions:
+    {
+      listenInfos:
+        [
+          {
+            protocol: 'udp',
+            ip: process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+            announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP,
+            port: 44444
+          },
+          {
+            protocol: 'tcp',
+            ip: process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+            announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP,
+            port: 44444
+          }
+        ],
     },
-  },
-} as const;
+    // mediasoup WebRtcTransport options for WebRTC endpoints (mediasoup-client,
+    // libmediasoupclient).
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
+    webRtcTransportOptions:
+    {
+      // listenIps is not needed since webRtcServer is used.
+      // However passing MEDIASOUP_USE_WEBRTC_SERVER=false will change it.
+      listenIps:
+        [
+          {
+            ip: process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+            announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP
+          }
+        ],
+      initialAvailableOutgoingBitrate: 1000000,
+      minimumAvailableOutgoingBitrate: 600000,
+      maxSctpMessageSize: 262144,
+      // Additional options that are not part of WebRtcTransportOptions.
+      maxIncomingBitrate: 1500000
+    },
+    // mediasoup PlainTransport options for legacy RTP endpoints (FFmpeg,
+    // GStreamer).
+    // See https://mediasoup.org/documentation/v3/mediasoup/api/#PlainTransportOptions
+    plainTransportOptions:
+    {
+      listenIp:
+      {
+        ip: process.env.MEDIASOUP_LISTEN_IP || '0.0.0.0',
+        announcedIp: process.env.MEDIASOUP_ANNOUNCED_IP
+      },
+      maxSctpMessageSize: 262144
+    }
+  }
+},
+} as const ;
